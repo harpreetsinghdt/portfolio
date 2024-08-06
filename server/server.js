c998d47086567a2b3d0859133ea5543191a5ea3a
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-08-06 13:03:25 -0400

    hostname add to listening port

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const path = require("path");
 dotenv.config();
 
 const app = express();
+
+const HOSTNAME = "0.0.0.0";
 const PORT = process.env.PORT || 5000;
 
 // MongoDB connection URL and database name
@@ -100,7 +102,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, HOSTNAME, () => {
   console.log(`Server is running at port: ${PORT}`);
 });
 

dc20971396d49d391bb8eaad10d044c4d698d572
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-08-06 13:37:49 -0400

    hostname removed to listening port

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 10000, // Ensure the port matches your deployment configuration
+    port: 10000,
   },
 });
--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ dotenv.config();
 
 const app = express();
 
-// const HOSTNAME = "0.0.0.0";
 const PORT = process.env.PORT || 5000;
 
 // MongoDB connection URL and database name

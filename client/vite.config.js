18c37b3f1800c616db400ccc88dcae32df960318
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-08-06 14:06:41 -0400

    dist folder path added to vite file

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../dist", // Adjust the output directory if needed
+    outDir: "dist", // Adjust the output directory if needed
   },
   server: {
     port: 10000, // Ensure the port matches your deployment configuration

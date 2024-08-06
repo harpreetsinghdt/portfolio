d68d105114d73e7097a1a7b61a4eda181e8dfbe6
Harpreet Singh <harpreetsinghdt@gmail.com>
2024-08-06 13:30:53 -0400

    hostname removed to listening port

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 
-const HOSTNAME = "0.0.0.0";
+// const HOSTNAME = "0.0.0.0";
 const PORT = process.env.PORT || 5000;
 
 // MongoDB connection URL and database name
@@ -102,7 +102,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-app.listen(PORT, HOSTNAME, () => {
+app.listen(PORT, () => {
   console.log(`Server is running at port: ${PORT}`);
 });
 
